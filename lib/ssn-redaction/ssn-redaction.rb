9c717d49fbdd749c0b56e91f1ba8564544f63d8e
Manuel Aristarán <jazzido@jazzido.com>
2014-06-21 18:05:00 -0500

    read pdf

--- lib/ssn-redaction/ssn-redaction.rb
+++ lib/ssn-redaction/ssn-redaction.rb
@@ -1,11 +1,13 @@
 require 'tabula'
 module SSNRedaction
 
-  def self.extract_text(filename, pages)
-    extractor = Tabula::Extraction::ObjectExtractor.new(filename, pages)
-
-
-
+  def self.get_matching_chunks(filename, page, pattern)
+    extractor = Tabula::Extraction::ObjectExtractor.new(filename, [page])
+    filtered = []
+    extractor.extract.inject([]) do |memo, pdf_page|
+      memo += Tabula::TextElement.merge_words(pdf_page.get_text).select do |chunk|
+        chunk.text =~ pattern
+      end
+    end
   end
-
 end
